5ddcb15b5cb712e1b68973ab934ff6b1ee4bda1b
Andrey Kavetski <andreykavecki@gmail.com>
2019-03-21 20:56:32 +0300

    fix: 3 tasks is complete

--- src/max-heap.js
+++ src/max-heap.js
@@ -35,8 +35,25 @@ class MaxHeap {
 
     }
 
+    equal(a, b) {
+        if (a.priority > b.priority) return 1;
+        else return -1;
+    }
+
     insertNode(node) {
-        this.root = node;
+        if (this.root == null) {
+            this.root = node;
+            this.parentNodes.push(node);
+        } else if (this.root.priority > node.priority) {
+            this.parentNodes.push(this.root);
+            this.parentNodes.sort(this.equal);
+            this.root = node;
+        } else {
+            this.parentNodes.push(node);
+            this.parentNodes.sort(this.equal);
+            this.root.appendChild(node);
+        }
+        console.log(this.parentNodes);
     }
 
     shiftNodeUp(node) {
--- src/node.js
+++ src/node.js
@@ -2,10 +2,14 @@ class Node {
     constructor(data, priority) {
         this.data = data;
         this.priority = priority;
+        this.left == null;
+        this.right == null;
     }
 
     appendChild(node) {
-
+        if (this.left == null) this.left = node;
+        else if (this.right == null) this.right = node;
+        else this.left.appendChild(node);
     }
 
     removeChild(node) {
